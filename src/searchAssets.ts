2ef0486ec5b7596383c2b3238344242dcad469c4
Shibko Aliaksandr <aliaksandr.shibko@gmail.com>
2020-08-28 11:29:37 +0300

    add feature to searchAssets

--- src/searchAssets.ts
+++ src/searchAssets.ts
@@ -17,7 +17,11 @@ function getUrl(
   }&client_secret=${body.Authentication.ClientSecret}&url=${host}`;
 }
 
-function getAssetDetailsJSON(filter_arr: Element[], assetType: string) {
+function getAssetDetailsJSON(
+  filter_arr: Element[],
+  assetType: string,
+  folderId: number
+) {
   if (filter_arr.length === 1) {
     return {
       assetType,
@@ -27,6 +31,7 @@ function getAssetDetailsJSON(filter_arr: Element[], assetType: string) {
       duplicateFlag: 0,
       missingFolderInTarget: 0,
       assetUpdatedAt: filter_arr[0].updatedAt,
+      folderId,
     };
   }
   if (filter_arr.length === 0) {
@@ -38,6 +43,7 @@ function getAssetDetailsJSON(filter_arr: Element[], assetType: string) {
       duplicateFlag: 0,
       missingFolderInTarget: 0,
       assetUpdatedAt: "",
+      folderId,
     };
   }
   if (filter_arr.length > 1) {
@@ -49,6 +55,7 @@ function getAssetDetailsJSON(filter_arr: Element[], assetType: string) {
       duplicateFlag: 1,
       missingFolderInTarget: 0,
       assetUpdatedAt: filter_arr[0].updatedAt,
+      folderId,
     };
   }
 
@@ -112,6 +119,21 @@ async function refreshTokens(
     .catch(() => null);
 }
 
+function getAccess(targetSiteId: number) {
+  return axios
+    .get<Access>(
+      `http://apps.portqii.com:8070/getTokens?siteId=${targetSiteId}`
+    )
+    .then(({ data }) => data)
+    .catch((e) => {
+      throw {
+        status: e.response.status,
+        message: e.message,
+        url: e.response.config.url,
+      };
+    });
+}
+
 export const index: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ) => {
@@ -121,59 +143,28 @@ export const index: APIGatewayProxyHandler = async (
     const page = 0;
     const assetDetailsArr = [];
 
-    const source = await axios
-      .get<Source>(
-        `http://apps.portqii.com:8070/getFolderDetails?folderId=${body.FolderDetails.folderId}&siteId=${body.SiteDetails.sourceSiteId}&assetType=${body.AssetDetails.assetType} `
-      )
-      .then(({ data }) => ({
-        sourceAbsolutePath: data.Absolute_Path,
-        sourceFolderName: data.Folder_Name,
-      }))
-      .catch((e) => {
-        throw {
-          status: e.response.status,
-          message: e.message,
-          url: e.response.config.url,
-        };
-      });
-
-    const folder = await axios
-      .get<Source>(
-        encodeURI(
-          `http://apps.portqii.com:8070/getTargetAbsolutePath?siteId=${body.SiteDetails.targetSiteId}&assetType=${body.AssetDetails.assetType}&absolutePath=${source.sourceAbsolutePath}&sourceFolderName=${source.sourceFolderName}`
-        )
-      )
-      .then(({ data }) => data);
-
-    if (folder) {
-      const target = {
-        targetAbsolutePath: folder.Absolute_Path,
-        targetFolderId: folder.Folder_Id,
-      };
-
-      const access = await axios
-        .get<Access>(
-          `http://apps.portqii.com:8070/getTokens?siteId=${body.SiteDetails.targetSiteId}`
-        )
-        .then(({ data }) => data)
-        .catch((e) => {
-          throw {
-            status: e.response.status,
-            message: e.message,
-            url: e.response.config.url,
-          };
-        });
-
-      const url = getUrl(
-        access.Base_Url,
-        access.Access_Token,
-        access.Refresh_Token,
-        body,
-        target.targetFolderId
+    if (body.FolderDetails.folderId === 0) {
+      const assetName = body.AssetDetails.assetName.replace(
+        /[^A-Za-z\d\s]/g,
+        "*"
       );
 
+      const access = await getAccess(body.SiteDetails.targetSiteId);
+
+      const host = `${access.Base_Url}/API/REST/2.0/assets/${
+        body.AssetDetails.assetApiName
+      }s?search=name=${encodeURIComponent(assetName)}`;
+
       let elementResponse = await axios
-        .get<Response>(`${url}${page + 1}`)
+        .get(
+          `https://m88a5j5z7k.execute-api.us-east-1.amazonaws.com/dev/request?access_token=${encodeURIComponent(
+            access.Access_Token
+          )}&refresh_token=${encodeURIComponent(
+            access.Refresh_Token
+          )}&client_id=${body.Authentication.ClientId}&client_secret=${
+            body.Authentication.ClientSecret
+          }&url=${host}`
+        )
         .then(({ data }) => {
           return {
             ...data.Response,
@@ -191,7 +182,7 @@ export const index: APIGatewayProxyHandler = async (
             elementResponse.AccessToken,
             elementResponse.RefreshToken,
             body,
-            target.targetFolderId
+            body.SiteDetails.targetSiteId
           ),
           page,
           elementResponse.AccessToken,
@@ -199,25 +190,149 @@ export const index: APIGatewayProxyHandler = async (
         );
       }
 
-      if (elementResponse && elementResponse.total >= 1001) {
-        const nextPage = await fetchNextPages(elementResponse, url);
+      if (elementResponse.elements.length === 1) {
+        const assetDetailsJSON = {
+          assetType: body.AssetDetails.assetType,
+          assetName: elementResponse.elements[0].name,
+          assetId: elementResponse.elements[0].id,
+          missingAssetInTarget: 0,
+          duplicateFlag: 0,
+          missingFolderInTarget: 0,
+          assetUpdatedAt: "",
+          targetFolderId: elementResponse.elements[0].folderId,
+          folderId: body.FolderDetails.folderId,
+        };
 
-        elementResponse.elements.push(...nextPage);
+        assetDetailsArr.push(assetDetailsJSON);
+      } else if (elementResponse.elements.length === 0) {
+        const assetDetailsJSON = {
+          assetType: body.AssetDetails.assetType,
+          assetName: "",
+          assetId: "",
+          missingAssetInTarget: 1,
+          duplicateFlag: 0,
+          missingFolderInTarget: 0,
+          assetUpdatedAt: "",
+          folderId: body.FolderDetails.folderId,
+        };
+
+        assetDetailsArr.push(assetDetailsJSON);
+      } else if (elementResponse.elements.length > 0) {
+        const assetDetailsJSON = {
+          assetType: body.AssetDetails.assetType,
+          assetName: elementResponse.elements[0].name,
+          assetId: elementResponse.elements[0].id,
+          missingAssetInTarget: 0,
+          duplicateFlag: 1,
+          missingFolderInTarget: 0,
+          assetUpdatedAt: "",
+          targetFolderId: elementResponse.elements[0].folderId,
+          folderId: body.FolderDetails.folderId,
+        };
+
+        assetDetailsArr.push(assetDetailsJSON);
       }
+    } else {
+      const source = await axios
+        .get<Source>(
+          `http://apps.portqii.com:8070/getFolderDetails?folderId=${body.FolderDetails.folderId}&siteId=${body.SiteDetails.sourceSiteId}&assetType=${body.AssetDetails.assetType} `
+        )
+        .then(({ data }) => ({
+          sourceAbsolutePath: data.Absolute_Path,
+          sourceFolderName: data.Folder_Name,
+        }))
+        .catch((e) => {
+          throw {
+            status: e.response.status,
+            message: e.message,
+            url: e.response.config.url,
+          };
+        });
 
-      if (elementResponse) {
-        const filter_arr = elementResponse.elements.filter(
-          (element) =>
-            element.type === body.AssetDetails.assetSearchName &&
-            element.name === body.AssetDetails.assetName
+      const folder = await axios
+        .get<Source>(
+          encodeURI(
+            `http://apps.portqii.com:8070/getTargetAbsolutePath?siteId=${body.SiteDetails.targetSiteId}&assetType=${body.AssetDetails.assetType}&absolutePath=${source.sourceAbsolutePath}&sourceFolderName=${source.sourceFolderName}`
+          )
+        )
+        .then(({ data }) => data);
+
+      if (folder) {
+        const target = {
+          targetAbsolutePath: folder.Absolute_Path,
+          targetFolderId: folder.Folder_Id,
+        };
+
+        const access = await getAccess(body.SiteDetails.targetSiteId);
+
+        const url = getUrl(
+          access.Base_Url,
+          access.Access_Token,
+          access.Refresh_Token,
+          body,
+          target.targetFolderId
         );
 
-        const assetDetailsJSON = getAssetDetailsJSON(
-          filter_arr,
-          body.AssetDetails.assetType
-        );
+        let elementResponse = await axios
+          .get<Response>(`${url}${page + 1}`)
+          .then(({ data }) => {
+            return {
+              ...data.Response,
+              AccessToken: data.AccessToken,
+              RefreshToken: data.RefreshToken,
+            };
+          })
+          .catch<null>(() => null);
 
-        if (assetDetailsJSON) {
+        if (elementResponse?.AccessToken && elementResponse?.RefreshToken) {
+          elementResponse = await refreshTokens(
+            body.SiteDetails.targetSiteId,
+            getUrl(
+              access.Base_Url,
+              elementResponse.AccessToken,
+              elementResponse.RefreshToken,
+              body,
+              target.targetFolderId
+            ),
+            page,
+            elementResponse.AccessToken,
+            elementResponse.RefreshToken
+          );
+        }
+
+        if (elementResponse && elementResponse.total >= 1001) {
+          const nextPage = await fetchNextPages(elementResponse, url);
+
+          elementResponse.elements.push(...nextPage);
+        }
+
+        if (elementResponse) {
+          const filter_arr = elementResponse.elements.filter(
+            (element) =>
+              element.type === body.AssetDetails.assetSearchName &&
+              element.name === body.AssetDetails.assetName
+          );
+
+          const assetDetailsJSON = getAssetDetailsJSON(
+            filter_arr,
+            body.AssetDetails.assetType,
+            target.targetFolderId
+          );
+
+          if (assetDetailsJSON) {
+            assetDetailsArr.push(assetDetailsJSON);
+          }
+        } else {
+          const assetDetailsJSON = {
+            assetType: body.AssetDetails.assetType,
+            assetName: "",
+            assetId: "",
+            missingAssetInTarget: 1,
+            duplicateFlag: 0,
+            missingFolderInTarget: 1,
+            folderId: target.targetFolderId,
+          };
+
           assetDetailsArr.push(assetDetailsJSON);
         }
       } else {
@@ -225,23 +340,13 @@ export const index: APIGatewayProxyHandler = async (
           assetType: body.AssetDetails.assetType,
           assetName: "",
           assetId: "",
-          missingAssetInTarget: 1,
-          duplicateFlag: 0,
-          missingFolderInTarget: 1,
+          targetFolderId: "",
+          missingFolderStructure: 1,
+          folderId: body.FolderDetails.folderId,
         };
 
         assetDetailsArr.push(assetDetailsJSON);
       }
-    } else {
-      const assetDetailsJSON = {
-        assetType: body.AssetDetails.assetType,
-        assetName: "",
-        assetId: "",
-        targetFolderId: "",
-        missingFolderStructure: 1,
-      };
-
-      assetDetailsArr.push(assetDetailsJSON);
     }
 
     return {
